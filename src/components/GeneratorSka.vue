2b5e3774ea1219c5c20e096521659c018c6f8f2e
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-04-15 10:47:48 +0300

    fix

---
+++ src/components/GeneratorSka.vue
@@ -0,0 +1,33 @@
+<template>
+  <button type="button" @click="rows--">-</button>
+  {{ rows }}
+  <button type="button" @click="rows++">+</button>
+  x
+  <button type="button" @click="columns--">-</button>
+  {{ columns }}
+  <button type="button" @click="columns++">+</button>)
+  
+  <table class="it">
+    <tr>
+      <td>
+        <button type="button" @click=""></button>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'GeneratorSka',
+    data() {
+      return {
+        rows: 2,
+        columns: 2,
+      };
+    }
+  }
+</script>
+
+<style>
+
+</style>

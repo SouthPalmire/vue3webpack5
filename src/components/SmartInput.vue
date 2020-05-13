6f8c2e507bcd84872ef3ab4a06aa88c53ab301a0
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-05-13 20:15:25 +0300

    fix

--- src/components/SmartInput.vue
+++ src/components/SmartInput.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="app" style="text-align: center;">  
+<div id="app" style="text-align: center;">
     <form>
       <input type="text" v-model="newInstruction.min" size="12" placeholder="min threshold" />
       <input type="text" v-model="newInstruction.max" size="12" placeholder="max threshold" />
@@ -9,7 +9,7 @@
         create
       </button> 
     </form>
-<div v-for="(index, instruction) in instructions" :key="instruction">
+<table v-for="(index, instruction) in instructions" :key="instruction">
     <tr>
       <td>{{index.min}}</td>
       <td>{{index.max}}</td>
@@ -17,12 +17,12 @@
       <td :style="{ color: index.color }">{{index.text}}</td>
       <td><button @click="instructions.pop()">delete</button></td>{{index}} {{instruction}}
     </tr>
-</div>
+</table>
     <div class="showcase">
       <input class="showcase__target" v-model="target" />
     </div>
 
-{{newInstruction}} {{instructions}} {{target}} 
+{{newInstruction}} {{instructions}} {{target}}
 
   </div>
 </template>
@@ -59,8 +59,8 @@
 </script>
 
 <style>
-  td {  
+  td {
     height: 25px;
-    width: 45px;
+    width: 60px;
   }
 </style>

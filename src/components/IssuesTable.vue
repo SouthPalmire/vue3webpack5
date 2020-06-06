e9f5bbb7cace6524588eaaacbf36e2402914a505
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-06-06 20:20:25 +0300

    fix

--- src/components/IssuesTable.vue
+++ src/components/IssuesTable.vue
@@ -1,21 +1,30 @@
 <template>
   <div>  
-    <div class="list" v-for="item in massive" :key="item.index">
-      
+    <div class="issue" v-for="item in massive" :key="item.index">  
       <div class="number">
         {{ item.number }}
       </div>
-      <div class="title">
-        {{ item.title }}
-      </div>
-      <div class="state">
-        {{ item.state }}
+      <form class="form">
+        <div class="title">
+          {{ item.title }}
+        </div>
+        <div class="state">
+          {{ item.state }}   
+        </div>
+        <div class="created">
+          {{ item.created_at }}
+        </div>
+        <button class="button">
+          <img class="arrow" src="http://localhost:8080/svg/down-arrow.svg" />
+        </button>
+      </form> 
+      <VueMarkdown class="body">
+        {{ item.body }}
+      </VueMarkdown> 
+      <div>
         <img class="state-out" v-if="item.state == 'closed'" src="http://localhost:8080/svg/tick.svg" />
         <img class="state-in" v-else src="http://localhost:8080/svg/dev.svg" />  
       </div>
-      <VueMarkdown class="body">
-        {{ item.body }}
-      </VueMarkdown>  
     </div>
   </div>
 </template>
@@ -35,30 +44,44 @@
     created() {
       fetch('https://api.github.com/repos/SouthPalmire/sandbox/issues?state=all')
         .then(response => response.json())  
-        .then(data => (this.massive = data.map(({title, state, number, body}) => ({title, state, number, body})))); 
+        .then(data => (this.massive = data.map(({title, state, number, body, created_at}) => ({title, state, number, body, created_at})))); 
     }
   }
 </script>
 
 <style>
-  .body {
-    text-align: left;
+  .input {
+    width: 15px;
+    height: 15px;
   }
-  .list { 
+  .arrow {
+    width: 15px;
+    height: 15px;
+  }
+  .form {
+    float: left;
+  }
+  .button {
+    float: left;
+  }
+  .created {
+    float: left;
+  }
+  .body {
+  }
+  .issue { 
   }
   .title {
-    margin-left: 10px;
     float: left;
   }
   .title::first-letter {
   text-transform: uppercase;
   }
   .number {
-    width: 20px;
     float: left;
   }
   .state {
-    text-align: right;
+    float: left;
   }
   .state-in, .state-out {
     width: 15px;

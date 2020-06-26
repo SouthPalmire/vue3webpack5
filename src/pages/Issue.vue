d22fcd036b4809c3ea62a5776eb3098d0c9704b8
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-06-26 18:12:02 +0300

    fix

---
+++ src/pages/Issue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Issue',
+    data() {
+      return {
+
+      }
+    },
+
+  }
+</script>
+
+<style>
+
+</style>

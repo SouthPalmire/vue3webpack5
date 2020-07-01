e35260c64d95c42439fa4d37d4c75a3ba48181d2
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-07-01 20:36:39 +0300

    fix

--- src/pages/Issue.vue
+++ src/pages/Issue.vue
@@ -1,31 +1,21 @@
 <template>
   <div class="main">
 
-    <h1><strong>#20 Comment me</strong></h1>
-    <p>Using <code>curl</code> command line tool create command under this issue</p>
-    <p><strong>Comments:</strong></p>
-
-    <div class="comments-massive" v-for="item in massive" :key="item">
-      <div class="comments-massive__body">
-        <VueMarkdown> {{ item.body }} </VueMarkdown>
-      </div>
-      <div class="comments-massive__created">
-        {{ item.created_at }}
-      </div>  
+    <div>
+     # {{ issue.number }} {{ issue.title }}
+    </div>
+    <div>
+      <VueMarkdown> {{ issue.body }} </VueMarkdown>
+      <p><strong>Comments:</strong></p>
     </div>
 
-    <div class="comments-newMassive" v-for="(newItem, newIndex) in newMassive" :key="newItem">
-      <div class="comments-newMassive__body">
-        <VueMarkdown> {{ newItem.body }} </VueMarkdown>
-      </div>
-      <div class="comments-newMassive__created">
-        {{ newItem.created_at }}
-      </div>
-      <button class="comments-newMassive__delete" @click='deleteComment(), deleteNewMassive(newIndex)'>Delete</button>
-    </div>
+    <div>
 
-    <input type="text" class="comments-newMassive__target" v-model='target' />
-    <button v-if='target' @click='createComment(), createNewMassive()'>Add</button>
+{{ comments }}
+    </div>
+    
+
+    
 
   </div>
 </template>
@@ -39,96 +29,22 @@
     },
     data() {
       return {
-        target: '',
-        massive: [],
-        newMassive: [],
+        issue: '',
+        comments: []
       }
     },
     created() {
+      fetch('https://api.github.com/repos/SouthPalmire/sandbox/issues/20')
+        .then(response => response.json())
+        .then(data => this.issue = data);
       fetch('https://api.github.com/repos/SouthPalmire/sandbox/issues/20/comments')
         .then(response => response.json())
-        .then(data => (this.massive = data.map(({body, created_at}) => ({body, created_at}))))
-    },
-
-    methods: {
-
-      createComment() {
-        let { target } = this;
-        const requestOptionsPush = {
-          method: 'POST',
-          headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '
-          },
-          body: JSON.stringify({ body: target }),
-        };
-        fetch('https://api.github.com/repos/SouthPalmire/sandbox/issues/20/comments', requestOptionsPush)
-        ;
-      },
-
-      deleteComment() {
-        const requestOptionsDelete = {
-          method: 'DELETE',
-          headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '
-          },
-          
-        };
-        fetch('https://api.github.com/repos/SouthPalmire/sandbox/issues/20/comments', requestOptionsDelete)
-        ;
-      },
-
-      createNewMassive() {
-        let { target: body } = this;
-        let created_at = new Date();
-        let massiveCreate = { body, created_at };
-        this.newMassive.push(massiveCreate);
-      },
-      
-      deleteNewMassive(newIndex) {
-        this.newMassive.splice(newIndex, 1);
-      }
+        .then(data => this.comments = data)
     }
   }
 </script>
 
 <style>
 
-  .main {
-
-  }
-
-  .comments-massive {
   
-  }
-
-  .comments-massive__body {
-  
-  }
-
-  .comments-massive__created {
-  
-  }
-
-  .comments-newMassive {
-
-  }
-
-  .comments-newMassive__body {
-
-  }
-
-  .comments-newMassive__created {
-
-  }
-
-  .comments-newMassive__delete {
-  
-  }
-
-  .comments-newMassive__target {
-  
-  }
-
 </style>

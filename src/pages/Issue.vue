7c1662cc75062c2495f5ebfea736beb7d05af1e1
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-07-02 17:59:36 +0300

    fix

--- src/pages/Issue.vue
+++ src/pages/Issue.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="main">
-
-    <h1>
-      <strong> # {{ issueHead.number }} {{ issueHead.title }}</strong>
-    </h1>
-    <div>
-      <VueMarkdown> {{ issueHead.body }} </VueMarkdown>
-      <p><strong>Comments:</strong></p>
+  <div>
+    
+    <div v-if="issueHead | comments == ''">
+      <p><code>Loading issue...</code></p>
+      <p><code>Loading comments...</code></p>
     </div>
 
-    <div class="comments" v-for="(comment, index) in comments" :key="index">
-      <div class="comments-body">
-        <VueMarkdown> {{ comment.body }} </VueMarkdown>
+    <div v-else>
+      <h1>
+        <strong>#{{ issueHead.number }} {{ issueHead.title }}</strong>
+      </h1>
+      <div>
+        <VueMarkdown> {{ issueHead.body }} </VueMarkdown>
+        <p><strong>Comments:</strong></p>
       </div>
-      <div class="comments-created">
-        {{ comment.created_at }}
-      </div>
-      <button class="comments-delete" v-if="comment.user.login == 'SouthPalmire'" @click='deleteComment(comment)'>Delete</button>
-    </div>
 
-    <input type="text" class="comments-target" v-model='target' />
-    <button class="comments-add" v-if='target' @click='createComment()'>Add</button>
+      <div class="comments" v-for="(comment, index) in comments" :key="index">
+        <div class="comments-body">
+          <VueMarkdown> {{ comment.body }} </VueMarkdown>
+        </div>
+        <div class="comments-created">
+          {{ comment.created_at }}
+        </div>
+        <button v-if="comment.user.login == 'SouthPalmire'" @click="deleteComment(comment)">Delete</button>
+      </div>
+
+      <textarea class="comments-target" v-model='target'></textarea><p/>
+      <button v-if='target' @click="createComment()">Add</button>
+    </div>
 
   </div>
 </template>
@@ -74,5 +81,29 @@
 </script>
 
 <style>
+
+  .comments {
+    display: flex;
+    overflow: auto;
+    flex-grow: 5;
+    align-items: center;
+    text-align: left;
+  }
+
+  .comments-body {
+    margin-left: 30px;
+    flex-grow: 1;
+  }
+
+  .comments-created {
+    margin-right: 10px;
+    flex-grow: 1; 
+    text-align: right;
+  }
+
+  .comments-target {
+    width: 700px;
+    height: 80px;
+  }
 
 </style>

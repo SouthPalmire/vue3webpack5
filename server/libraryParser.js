bd51f87107a9c6c7729a7faf1257d82dd9ebce0b
Your Name <hexus.southpalmire@gmail.com>
2020-08-02 09:23:28 +0300

    fix

--- server/libraryParser.js
+++ server/libraryParser.js
@@ -8,24 +8,15 @@ async function getPageData() {
    let pageData = []
    const res = await fetch('http://lib.ru/RUFANT/')
    const blok = await res.text()
-
    // const full = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/g
-  
-   // let result = full.exec(blok)
 
-   const { groups: {type, uri, name}} = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/g.exec(blok)
-   const newPageData = {type, uri, name}
-   // console.log(`name: ${name}, type: ${type}, uri: ${uri}`);
-   // pageData.push(`${name}`, `${type}`, `${uri}`)
+   const { groups: { type, uri, name }} = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/g.exec(blok)
+   const newPageData = { name, type, uri }
+
    pageData.push(newPageData)
    console.log(pageData)
-   // console.log(cosso)
 
    // console.log(blok)
-
-   // console.log(result.groups.type)
-   // console.log(result.groups.uri)
-   // console.log(result.groups.name)
 
 }
 

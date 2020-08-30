3df4130195b1934da41c98efdadfa6378a5b8ff6
HexusSouthpalmire <hexus.southpalmire@gmail.com>
2020-08-30 17:18:29 +0300

    fix

--- server/77.js
+++ server/77.js
@@ -1,6 +1,27 @@
 // const EventEmitter = require('events');
-// const fetch = require('node-fetch');
-// const iconv = require('iconv-lite');
+const fetch = require('node-fetch');
+const iconv = require('iconv-lite');
+
+
+async function getPageData() {
+
+
+    const data = await fetch('http://lib.ru/RUFANT/')
+        .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
+    
+    data.on("data", function(chunk){ 
+        const autors = []
+        autors.push(chunk);
+        console.log(autors);
+    });
+    
+    // console.log(data)
+}
+getPageData()
+
+
+
+
 
 // class ReadlineInterface extends EventEmitter {
 //     constructor(input) {

7079520c364b16f743117271e7ad2e86dd1d0401
SouthPalmire <hexus.southpalmire@gmail.com>
2020-11-26 15:08:42 +0300

    fix

--- server/6.js
+++ server/6.js
@@ -5,7 +5,6 @@ const iconv = require('iconv-lite');
 const input = async () => {
     const answer = await fetch('http://lib.ru/RUFANT/')
         .then(response => response.body.pipe(iconv.decodeStream('koi8-ru')))
-        .then(data => data.txt())
     console.log(answer)
 }
 input()

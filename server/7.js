cb3a4c85bb44f2c50eda9061bfa28fc2ba4b1be6
SouthPalmire <hexus.southpalmire@gmail.com>
2020-12-04 16:26:37 +0300

    fix

--- server/7.js
+++ server/7.js
@@ -6,7 +6,17 @@ class ReadlineInterface extends EventEmitter {
     constructor(input) {
         super()
         this.input = input
-        // console.log(input)
+
+        .on('data', (chunk) => {
+            const streamToString = chunk.toString()
+            const splitedString = streamToString.split('\n')
+            this.emit('line', splitedString)
+        })
+        
+        .on('end', () => {
+            this.emit('close')
+        })
+
     }
 }
 
@@ -15,3 +25,20 @@ const readline = {
         return new ReadlineInterface(input)
     }
 };
+
+(async () => {
+    const data = await fetch('http://lib.ru/RUFANT/')
+        .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
+    const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
+    const autors = []
+    const rl = readline.createInterface({ input: data });
+    rl.on('line', (input) => {
+        const result = regular.exec(input)       
+        if (result) {  
+            const { name, type, uri } = result.groups
+            const newObject = { name, type, uri }
+            autors.push(newObject)
+        } 
+    }) 
+    .on('close', () => console.log(autors))
+})()

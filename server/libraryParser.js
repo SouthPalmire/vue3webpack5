fd64b9f6f14bb12b2c1de26095eafcfd265d28e7
SouthPalmire <hexus.southpalmire@gmail.com>
2020-12-17 00:02:24 +0300

    fix

--- server/libraryParser.js
+++ server/libraryParser.js
@@ -1,68 +1,54 @@
-const readline = require('readline');
-const fetch = require('node-fetch');
-const iconv = require('iconv-lite');
+const EventEmitter = require('events')
+const fetch = require('node-fetch')
+const iconv = require('iconv-lite')
 
+class ReadlineInterface extends EventEmitter {
+    constructor(input) {
+        super()
+        let brokenString
+        input.on('data', (chunk) => {
+            const bufferToString = chunk.toString()
+            if (bufferToString.indexOf('\n') !== -1) {
+                bufferToString.split('\n').forEach((element, index, array) => {
+                    if (index === array.length -1) {
+                        brokenString = element
+                    } 
+                    else if (index === 0) {
+                        this.emit('line', brokenString + element)
+                    } 
+                    else { 
+                        this.emit('line', element) 
+                    }
+                })
+                // this.emit('line', brokenString)
+            }
+        })
+        
+        .on('end', () => {
+            this.emit('close')
+        })
+    }
+}
 
-// (async () => {
-//     const data = await fetch('http://lib.ru/RUFANT/')
-//         .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
-//     const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
-//     const autors = []
-//     const rl = readline.createInterface({ input: data });
-//     rl.on('line', (input) => {
-//         const result = regular.exec(input)       
-//         if (result) {  
-//             const { name, type, uri } = result.groups
-//             const newObject = { name, type, uri }
-//             autors.push(newObject)
-//         } 
-//     }) 
-//     .on('close', () => console.log(autors))
-// })()
+const readline = {
+    createInterface({ input }) {
+        return new ReadlineInterface(input)
+    }
+};
 
-
-
-
-async function getPageData() {
+(async () => {
+    const data = await fetch('http://lib.ru/RUFANT/')
+        .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
     const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
     const autors = []
-
-    const data = await fetch('http://lib.ru/RUFANT/')
-        .then(response => response.body.pipe(iconv.decodeStream('koi8-ru')))
-    
-    const rl = readline.createInterface({
-        input: data
-    });
-              
+    const rl = readline.createInterface({ input: data });
     rl.on('line', (input) => {
         const result = regular.exec(input)       
         if (result) {  
             const { name, type, uri } = result.groups
             const newObject = { name, type, uri }
             autors.push(newObject)
-        }
-    })
-
-    .on('close', () => {
-        console.log(autors)
-    })
-}
-getPageData()
-
-
-
-// const http = require('http')
- 
-// http.get("http://lib.ru/RUFANT/", function(res) {
-//    res.pipe(iconv.decodeStream('koi8-ru')).collect(function(err, decodedBody) {
-//       let autors = []
-//       let result
-//       const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/g
-//       while (result = regular.exec(decodedBody)) {
-//          const { name, type, uri } = result.groups
-//          const newObject = { name, type, uri }
-//          autors.push(newObject)
-//       }
-//       console.log(autors)
-//    });  
-// });
+        } 
+    }) 
+    .on('close', () => console.log(autors))
+})()
---
+++ server/testAll.js
@@ -0,0 +1,68 @@
+const readline = require('readline');
+const fetch = require('node-fetch');
+const iconv = require('iconv-lite');
+
+
+// (async () => {
+//     const data = await fetch('http://lib.ru/RUFANT/')
+//         .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
+//     const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
+//     const autors = []
+//     const rl = readline.createInterface({ input: data });
+//     rl.on('line', (input) => {
+//         const result = regular.exec(input)       
+//         if (result) {  
+//             const { name, type, uri } = result.groups
+//             const newObject = { name, type, uri }
+//             autors.push(newObject)
+//         } 
+//     }) 
+//     .on('close', () => console.log(autors))
+// })()
+
+
+
+
+async function getPageData() {
+    const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
+    const autors = []
+
+    const data = await fetch('http://lib.ru/RUFANT/')
+        .then(response => response.body.pipe(iconv.decodeStream('koi8-ru')))
+    
+    const rl = readline.createInterface({
+        input: data
+    });
+              
+    rl.on('line', (input) => {
+        const result = regular.exec(input)       
+        if (result) {  
+            const { name, type, uri } = result.groups
+            const newObject = { name, type, uri }
+            autors.push(newObject)
+        }
+    })
+
+    .on('close', () => {
+        console.log(autors)
+    })
+}
+getPageData()
+
+
+
+// const http = require('http')
+ 
+// http.get("http://lib.ru/RUFANT/", function(res) {
+//    res.pipe(iconv.decodeStream('koi8-ru')).collect(function(err, decodedBody) {
+//       let autors = []
+//       let result
+//       const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/g
+//       while (result = regular.exec(decodedBody)) {
+//          const { name, type, uri } = result.groups
+//          const newObject = { name, type, uri }
+//          autors.push(newObject)
+//       }
+//       console.log(autors)
+//    });  
+// });

1001e9bc137b105e8c751d2ec8d0ecebd5967ae6
SouthPalmire <hexus.southpalmire@gmail.com>
2020-12-17 20:56:48 +0300

    fix

--- server/libraryParser.js
+++ server/libraryParser.js
@@ -41,16 +41,16 @@ const readline = {
 (async () => {
     const data = await fetch('http://lib.ru/RUFANT/')
         .then(response => response.body.pipe(iconv.decodeStream('koi8-r')))
-    const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/   
+    const regular = /(?<=<small><b>)(?<type>.*?)(?=<\/b>).*?(?<=<\/small><\/tt> <A HREF=)(?<uri>.*?)(?=\/>).*?(?<=\/><b>)(?<name>.*?)(?=<\/b><\/A>)/
     const autors = []
     const rl = readline.createInterface({ input: data });
     rl.on('line', (input) => {
-        const result = regular.exec(input)       
+        const result = regular.exec(input)
         if (result) {  
             const { name, type, uri } = result.groups
             const newObject = { name, type, uri }
             autors.push(newObject)
-        } 
-    }) 
+        }
+    })
     .on('close', () => console.log(autors))
 })()

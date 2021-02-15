a44d9f6c523cfc4f844c2759fdcaa52ef02f841a
SouthPalmire <hexus.southpalmire@gmail.com>
2021-02-15 15:23:32 +0300

    fix

--- server/users.js
+++ server/users.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser");
 const mysql = require('mysql2');
 const sha1 = require('sha1');
 
+const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -10,85 +14,75 @@ var connection = mysql.createConnection({
     database : 'testDB'
 });
 
-// const pool = mysql.createPool({
-//     connectionLimit: 5,
-//     host: "localhost",
-//     user: "root",
-//     database: "testDB",
-//     password: "5399"
-//   });
-
-connection.connect((err) => {
-    if(err) {
-        console.log(err);
-        return err;
-    } else {
-        console.log('sql connection')
-    }
-})
-
-const app = express();
-
-const urlencodedParser = bodyParser.urlencoded({extended: false});
- 
 app.get("/register", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/register.html");
 });
+
 app.post("/register", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
+    if (!request.body) response.sendStatus(400);
 
     if(request.body.firstname ==='') {
         response.send('please enter your firstname');
         console.log('please enter your firstname');
     }
 
-    if(request.body.lastname ==='') {
+    if (request.body.lastname ==='') {
         response.send('please enter your lastname');
         console.log('please enter your lastname');
     }
 
-    if(request.body.password !== request.body.confirm_password) {
+    if (request.body.password !== request.body.confirm_password) {
         response.send('wrong password');
         console.log('wrong password');
     }
 
-    if(request.body.password === '') {
+    if (request.body.password === '') {
         response.send('please enter your password');
         console.log('please enter your password');
     }
 
-    if(request.body.email === '') {
+    if (request.body.email === '') {
         response.send('please enter your e-mail');
         console.log('please enter your e-mail');
     }
 
-    if(request.body.email.indexOf('@') === -1) {
+    if (request.body.email.indexOf('@') === -1) {
         response.send('wrong e-mail');
         console.log('wrong e-mail');
     }
 
-    if(request.body.date_of_birth === '') {
+    if (request.body.date_of_birth === '') {
         response.send('plese enter your date of birth');
         console.log('plese enter your date of birth');
     }
-    
-    console.log(sha1(request.body.password));
-    response.send(JSON.stringify(request.body));
+
+    const firstname = request.body.firstname;
+    const lastname = request.body.lastname;
+    const password = sha1(request.body.password);
+    const email =  request.body.email;
+    const date_of_birth = request.body.date_of_birth.split('-').join('');
+
+    connection.query("INSERT INTO user (firstname, lastname, password, email, date_of_birth, registration_date) VALUES (?,?,?,?,?,UNIX_TIMESTAMP())", [firstname, lastname, password, email, date_of_birth], function(err, data) {
+        if (err) console.log(err);
+        console.log('accepted');
+    });
+
+    connection.query("SELECT * FROM user WHERE id = LAST_INSERT_id()", function(err, data) {
+        if(err) console.log(err);
+        response.send(JSON.stringify(data));
+    });
 });
 
-// const wer = 'SELECT * FROM someone';
-
-// const qwe = connection.query(wer, (err, result) => {
-//     console.log(err);
-//     console.log(result);
-//     return result;
-// });
-
-
-app.get("/", function(request, response){
-    response.send();
+app.get("/", function(request, response) {
+    connection.query("SELECT * FROM user", function(err, data) {
+        if (err) {
+            console.log(err);
+            response.send(err);
+        }
+        response.send(JSON.stringify(data));
+    });
 });
   
 app.listen(1337, () => {
-    console.log('............')
+    console.log('online')
 });

b2921911b30c410ddacc42838fcf4de457326967
SouthPalmire <hexus.southpalmire@gmail.com>
2021-03-22 12:02:23 +0300

    fix

---
+++ issue34/public/app.vue
@@ -0,0 +1,27 @@
+<template>
+   <div id="app">
+   <!-- <router-view /> -->
+    {{ some }}
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'app',
+   data() {
+      return {
+         some: 'ooooooooo'
+      }
+   }
+}
+</script>
+
+<style>
+#app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   color: #2c3e50;
+   background: blue;
+}
+</style>

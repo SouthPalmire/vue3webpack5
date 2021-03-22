dcff899dd4945ffbd36c7f13bfe4745b1c0e93b4
SouthPalmire <hexus.southpalmire@gmail.com>
2021-03-22 14:57:09 +0300

    fix

---
+++ issue34/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="text">
+      {{ text }}
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'app',
+   data() {
+      return {
+         text: 'fffffffffff'
+      }
+   }
+}
+</script>
+
+<style>
+   #text {
+      color: blue;
+   }
+</style>

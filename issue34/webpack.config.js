914571326ae3fdad55cb0124d27f8397a25cc27f
SouthPalmire <hexus.southpalmire@gmail.com>
2021-03-22 12:49:05 +0300

    fix

--- issue34/webpack.config.js
+++ issue34/webpack.config.js
@@ -1,2 +1,29 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+   entry: path.join(__dirname, 'public', main.js),
+   output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/dist/',
+      filename: 'build.js'
+   },
+   module: {
+      rules: [
+         {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+         },
+         {
+            test: /\.css$/,
+            use: [
+               'vue-style-loader',
+               'css-loader'
+            ]
+         }
+      ]
+   },
+   plugins: [
+      new VueLoaderPlugin()
+   ]
+}

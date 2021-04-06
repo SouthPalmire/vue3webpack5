32968fc25141872bd3cf531fbba08dadeb7eefe6
SouthPalmire <hexus.southpalmire@gmail.com>
2021-04-06 09:25:10 +0300

    fix

--- vue3webpack5/server.js
+++ vue3webpack5/server.js
@@ -22,14 +22,14 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/dist'))
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
 
 app.get("/", function (request, response) {
-    response.sendFile(__dirname + "/dist/index.html")
+    response.sendFile(__dirname + '/dist/index.html')
 });
 
 app.post('/api*', (req, res) => {    
